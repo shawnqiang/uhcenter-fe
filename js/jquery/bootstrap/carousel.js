aa9f7a5709d9fd2e5292bb0d4fc98a9e0e918f07
wangshishuo <wangshishuo@gmail.com>
2012-01-07 16:14:13 +0800

    carousel cc

--- js/jquery/bootstrap/carousel.js
+++ js/jquery/bootstrap/carousel.js
@@ -6,6 +6,8 @@
     var _this = this;
     this.settings = $.extend({}, $.fn.carousel.defaults, options);
     this.wrapper = $('.carousel', content);
+    this.forwardBtn = $(content).parent().find('.carousel-forward');
+    this.backwardBtn = $(content).parent().find('.carousel-backward');
     if (this.settings.carousel == 'horizontal') {
       if (this.wrapper[0].offsetWidth > content.offsetWidth) {
         if (this.settings.wrap) {
@@ -27,16 +29,18 @@
         this.wholeSize = parseInt(this.wrapper.css('height'));
       }
     }
-    $(content).parent().find('.carousel-forward').click(function() {_this.forward.call(_this);});
-    $(content).parent().find('.carousel-backward').click(function() {_this.backward.call(_this);});
+    this.forwardBtn.click(function() {_this.forward.call(_this);});
+    this.backwardBtn.click(function() {_this.backward.call(_this);});
   }
 
   Carousel.prototype = {
     forward: function() {
       scroll.call(this, this.settings.step);
+      return false;
     },
     backward: function() {
       scroll.call(this, - this.settings.step);
+      return false;
     }
   }
 
@@ -44,18 +48,31 @@
    * ========================= */
   function scroll(step) {
     if (! this.scrollable) return;
+    var _this = this;
     if (this.settings.wrap) {
-      
-    } else {
-      var posi = pos.call(this) - step * this.itemSize;
-      pos.call(this, posi);
+      if (step < 0 && pos.call(this) == 0)
+        pos.call(this, - this.wholeSize / 2);
+      if (step > 0 && this.viewableSize - pos.call(this) == this.wholeSize)
+        pos.call(this, this.viewableSize - this.wholeSize / 2);
     }
+    var posi = pos.call(this) - step * this.itemSize;
+    scrollTo.call(this, posi, function() {
+      if (_this.settings.wrap) return;
+      posi == 0 ? _this.backwardBtn.addClass('disabled').attr('disabled', true) : _this.backwardBtn.removeClass('disabled').attr('disabled', false);
+      posi <= _this.viewableSize - _this.wholeSize ? _this.forwardBtn.addClass('disabled').attr('disabled', true) : _this.forwardBtn.removeClass('disabled').attr('disabled', false);
+    });
   }
 
   function pos(value) {
     return this.settings.carousel == 'horizontal' ?
-      value ? this.wrapper.css('left', value) : parseInt(this.wrapper.css('left')) :
-      value ? this.wrapper.css('top', value) : parseInt(this.wrapper.css('top'));
+      value!=null ? this.wrapper.css('left', value) : parseInt(this.wrapper.css('left')) :
+      value!=null ? this.wrapper.css('top', value) : parseInt(this.wrapper.css('top'));
+  }
+
+  function scrollTo(value, callback) {
+    return this.settings.carousel == 'horizontal' ?
+      this.wrapper.animate({'left': value}, 'slow', 'easeOutCubic', callback) :
+      this.wrapper.animate({'top': value}, 'slow', 'easeOutCubic', callback);
   }
 
   /* CAROUSEL PLUGIN DEFINITION
